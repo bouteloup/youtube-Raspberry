7539c2e71f6915f9e4ed3cdbdd2f4adf2c1e7596
root <root@raspberrypi.(none)>
2015-05-18 17:20:10 +0200

    Optimisé

--- main.sh
+++ main.sh
@@ -1,48 +1,7 @@
 #!/bin/bash
 
 #Test le contenu des paramètres. Lance la vidéo Yt si OK.
-if [ "-n" "$1" ]
-then
-	omxplayer -o hdmi $(youtube-dl -t -f 22 -g $1)
-fi
-
-if [ "-n" "$2" ]
-then
-	omxplayer -o hdmi $(youtube-dl -t -f 22 -g $2)
-fi
-
-if [ "-n" "$3" ]
-then
-	omxplayer -o hdmi $(youtube-dl -t -f 22 -g $3)
-fi
-
-if [ "-n" "$4" ]
-then
-	omxplayer -o hdmi $(youtube-dl -t -f 22 -g $4)
-fi
-
-if [ "-n" "$5" ]
-then
-	omxplayer -o hdmi $(youtube-dl -t -f 22 -g $5)
-fi
-
-if [ "-n" "$6" ]
-then
-	omxplayer -o hdmi $(youtube-dl -t -f 22 -g $6)
-fi
-
-if [ "-n" "$7" ]
-then
-	omxplayer -o hdmi $(youtube-dl -t -f 22 -g $7)
-fi
-
-if [ "-n" "$8" ]
-then
-	omxplayer -o hdmi $(youtube-dl -t -f 22 -g $8)
-fi
-
-if [ "-n" "$9" ]
-then
-	omxplayer -o hdmi $(youtube-dl -t -f 22 -g $9)
-fi
-
+for param in "$@"
+do
+	omxplayer -o hdmi $(youtube-dl -t -f 22 -g $param)
+done
